cfaab516de3553de39b545c569700351fe187c0a
Antoine Balaine <abalaineaccts@gmail.com>
2024-01-03 21:34:20 -0500

    lvim: add refactoring remaps

--- .config/lvim/lua/user/whichkey_mappings.lua
+++ .config/lvim/lua/user/whichkey_mappings.lua
@@ -7,3 +7,22 @@ lvim.builtin.which_key.mappings["t"] = {
     l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
     r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
 }
+
+local refactor_nvim = require('refactoring')
+-- Extract block supports only normal mode
+lvim.builtin.which_key.vmappings["r"] = {
+    name = "Refactor",
+    {
+        f = { function() refactor_nvim.refactor('Extract Function') end, "Extract Function" },
+        F = { function() refactor_nvim.refactor('Extract Function To File') end, "Extract Function To File" },
+        v = { function() refactor_nvim.refactor('Extract Variable') end, "Extract Variable" },
+        i = { function() refactor_nvim.refactor('Inline Variable') end, "Inline Variable" },
+    }
+}
+lvim.builtin.which_key.mappings["r"] = {
+    name = "Refactor",
+    f = { function() refactor_nvim.refactor('Inline Function') end, "Inline Function" },
+    v = { function() refactor_nvim.refactor('Inline Variable') end, "Inline Variable" },
+    b = { function() refactor_nvim.refactor('Extract Block') end, "Extract Block" },
+    B = { function() refactor_nvim.refactor('Extract Block To File') end, "Extract Block To File" },
+}
