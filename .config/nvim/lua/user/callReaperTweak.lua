0db579da9942ef96a03d50737c9d86d0ca734878
Anthony Balaine <abalaine@nexient.com>
2024-08-23 16:59:19 -0400

    nvim: lazy.nvim update config

---
+++ .config/nvim/lua/user/callReaperTweak.lua
@@ -0,0 +1,4 @@
+local reaper_tweak = "_RS7e337b0d36f252d3c446c3d79fe7b42e9301359e"
+local perken_rack = "_RSb038b231a657afb36aa0a3ee30744ce0cad482d3"
+reaper.Main_OnCommand(reaper.NamedCommandLookup(perken_rack), 0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup(perken_rack), 0)
