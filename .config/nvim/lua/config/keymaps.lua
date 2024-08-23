37aa9943be52a4a6ac46b2581360eaf21b279cb6
Anthony Balaine <abalaine@nexient.com>
2024-08-23 11:00:43 -0400

    nvim: lazy.nvim setup

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+vim.api.nvim_set_keymap("n", "<C-PageUp>", ":BufferLineCyclePrev<cr>", { silent = false })
+vim.api.nvim_set_keymap("n", "<C-PageDown>", ":BufferLineCycleNext<cr>", { silent = false })
+vim.api.nvim_set_keymap("n", "<S-j>", ":join<cr>", { silent = false })
