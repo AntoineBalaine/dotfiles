e413d89eaa952f8d78c3e886009494773ec61e6a
Anthony Balaine <abalaine@nexient.com>
2024-12-18 10:37:50 -0500

    nvim: autocmds, disable some plugins, copilotchat

---
+++ .config/nvim/lua/config/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  -- { "folke/trouble.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
+}
