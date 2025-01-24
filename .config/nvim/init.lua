0d68155d0c371b2e31b17f8a75ad50d7d22d60ee
Anthony Balaine <abalaine@nexient.com>
2025-01-24 09:52:36 -0500

    configs: nvim + rc files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("user.removegetters")
+require("user.transform_setters")
 require("user.helpers")
 require("user.lua_adventures")
+
+-- Create a command to call the function
+vim.api.nvim_create_user_command("TransformFn", transform_function_declaration, {})
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,33 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "tex",
   command = "setlocal makeprg=tectonic\\ %",
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zig",
+  callback = function()
+    vim.keymap.set("n", "<leader>zb", function()
+      require("zig-comp-diag").runWithCmd({ "zig", "build", "-Dtest=true" })
+    end, { buffer = true, desc = "Zig build" })
+
+    vim.keymap.set("n", "<leader>zt", function()
+      require("zig-comp-diag").runWithCmd({ "zig", "test", vim.fn.expand("%") })
+    end, { buffer = true, desc = "Zig test current file" })
+
+    vim.keymap.set("n", "<leader>zT", function()
+      require("zig-comp-diag").runWithCmd({ "zig", "build", "test" })
+    end, { buffer = true, desc = "Zig build test" })
+
+    vim.keymap.set("n", "<leader>zr", function()
+      require("zig-comp-diag").runWithCmd({
+        "zig",
+        "build",
+        "-Dtest=true",
+        "--prefix",
+        '"/Users/a266836/Library/Application Support/REAPER/UserPlugins"',
+        "&&",
+        "/Applications/REAPER.app/Contents/MacOS/REAPER",
+        "new",
+      })
+    end, { buffer = true, desc = "Zig install reaper" })
+  end,
+})
